325f7cd1753ac70cbce2bb42e0ec77f6910724ba
LAPTOP-0DFODGAS\Admin <malili2003li@gmail.com>
2024-01-20 00:16:12 +0300

    Finally added 16x16 matrix with video and image format convertion to it

--- examples/Matrix/modules/convert video/convert.py
+++ examples/Matrix/modules/convert video/convert.py
@@ -17,16 +17,7 @@ def process_video(video_path):
         if not ret:
             break
         
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # resized_frame = gray_frame
-        resized_frame = cv2.resize(gray_frame, (16, 16))
-        thresholded_frame = cv2.threshold(resized_frame, 80, 255, cv2.THRESH_BINARY)[1]
-        thresholded_frame = np.where(thresholded_frame > 100, 1, 0)  # Convert values to 0 or 1
-        A0, A1, A2, A3 = split_arrays(thresholded_frame)
-        STR[0] += '\n'+arr_to_str_frame(A0)
-        STR[1] += '\n'+arr_to_str_frame(A1)
-        STR[2] += '\n'+arr_to_str_frame(A2)
-        STR[3] += '\n'+arr_to_str_frame(A3)
+        resized_frame, thresholded_frame = append_str_frame(frame)
         if not fl:
             print(resized_frame)
             print(thresholded_frame)
@@ -38,12 +29,27 @@ def process_video(video_path):
     cap.release()
     return frames
 
-global STR
+def append_str_frame(frame):
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # resized_frame = gray_frame
+    resized_frame = cv2.resize(gray_frame, (16, 16))
+    thresholded_frame = cv2.threshold(resized_frame, 80, 255, cv2.THRESH_BINARY)[1]
+    thresholded_frame = np.where(thresholded_frame > 100, 1, 0)  # Convert values to 0 or 1
+    A0, A1, A2, A3 = split_arrays(thresholded_frame)
+    STR[0] += '\n'+arr_to_str_frame(A0)
+    STR[1] += '\n'+arr_to_str_frame(A1)
+    STR[2] += '\n'+arr_to_str_frame(A2)
+    STR[3] += '\n'+arr_to_str_frame(A3)
+    return resized_frame,thresholded_frame
+
+global STR, STR_BASE_LEN
 STEP = 100 #n
 STR = \
-[""".define Count8 
+[f""".define STR{i} 
  +0ns 00
 """ for i in range(4)]
+STR_BASE_LEN = len(STR[0])
+
 
 def byte_to_displacement(b, wait=STEP):
     S = hex(b)[2:].upper()
@@ -65,9 +71,13 @@ def get_arr_byte(arr, i):
     # for 2d binary (0/1) array
     I = i
     b = 0
+    if I==2:
+        print(arr)
+        print([arr[i][I] for i in range(len(arr))])
     for i in range(len(arr)):
         if arr[i][I]==1:
             b |= (1<<i)
+    print(b)
     return b
 
 def arr_to_str_frame(arr):
@@ -77,9 +87,12 @@ def arr_to_str_frame(arr):
     n = len(arr)
     print(n)
     for i in range(n):
+        step = STEP*n
+        if (i==0) and (len(STR[3])==STR_BASE_LEN):
+            step=0
         s = byte_to_displacement(
                 get_arr_byte(arr,i),
-                STEP*n
+                step
             ) + '\n'
         S += s
     return S
@@ -87,22 +100,26 @@ def arr_to_str_frame(arr):
 def split_arrays(original_array):
     splitted_arrays = [
         original_array[:8, :8],   # Array 1
-        original_array[:8, 8:],    # Array 3
         original_array[8:, :8],    # Array 2
+        original_array[:8, 8:],    # Array 3
         original_array[8:, 8:]     # Array 4
     ]
     return splitted_arrays
 
-if __name__ == '__main__':
-    video_path = "C:/Users/Admin/Documents/MCTest/examples/Matrix/modules/convert video/0001-0159.avi"
-    file_path  = "C:/Users/Admin/Documents/MCTest/examples/Matrix/modules/convert video/frames/STR{0}.txt"
-    save_path  = "C:/Users/Admin/Documents/MCTest/examples/Matrix/modules/convert video/photo"
-    resulting_frames = process_video(video_path)
 
+def save_str():
+    global STR
+    file_path  = "C:/Users/Admin/Documents/MCTest/examples/Matrix/modules/convert video/frames/STR{}.txt"
     for i in range(4):
         f = open(file_path.format(i), 'w')
         f.write(STR[i])
         f.close()
+
+if __name__ == '__main__':
+    video_path = "C:/Users/Admin/Documents/MCTest/examples/Matrix/modules/convert video/0001-0159.avi"
+    save_path  = "C:/Users/Admin/Documents/MCTest/examples/Matrix/modules/convert video/photo"
+    resulting_frames = process_video(video_path)
+
     i = 0
     for frame in resulting_frames:
         i += 1
